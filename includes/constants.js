50d5b2df72a58228c07590523138fd2691138506
Anup Kumar  Bal <anup.bal@zessta.com>
2024-11-25 07:40:44 +0000

    dynamically fetching data from airflow

--- includes/constants.js
+++ includes/constants.js
@@ -14,6 +14,22 @@ const SEPT = {
     FILE_FORMAT: "parquet"
 };
 
+const EXISTING_CONST = {
+    TABLE_NAME: "{{ task_instance.xcom_pull('create_workflow_invocation')['invocation_config']['parameters']['existing_name'] }}",
+    DATASET_NAME: "{{ task_instance.xcom_pull('create_workflow_invocation')['invocation_config']['parameters']['existing_dataset_name'] }}",
+    FOLDER_PATH: "{{ task_instance.xcom_pull('create_workflow_invocation')['invocation_config']['parameters']['existing_folder_path'] }}",
+    FILE_NAME: "{{ task_instance.xcom_pull('create_workflow_invocation')['invocation_config']['parameters']['existing_file_name'] }}",
+    FILE_FORMAT: "{{ task_instance.xcom_pull('create_workflow_invocation')['invocation_config']['parameters']['existing_file_format'] }}"
+};
+
+const INCOMING_CONST = {
+    TABLE_NAME: "{{ task_instance.xcom_pull('create_workflow_invocation')['invocation_config']['parameters']['incoming_name'] }}",
+    DATASET_NAME: "{{ task_instance.xcom_pull('create_workflow_invocation')['invocation_config']['parameters']['incoming_dataset_name'] }}",
+    FOLDER_PATH: "{{ task_instance.xcom_pull('create_workflow_invocation')['invocation_config']['parameters']['incoming_folder_path'] }}",
+    FILE_NAME: "{{ task_instance.xcom_pull('create_workflow_invocation')['invocation_config']['parameters']['incoming_file_name'] }}",
+    FILE_FORMAT: "{{ task_instance.xcom_pull('create_workflow_invocation')['invocation_config']['parameters']['incoming_file_format'] }}"
+};
+
 const COLUMNS = [
     "streetno",
     "name",
@@ -29,6 +45,8 @@ const COLUMNS = [
 ];
 
 module.exports = {
+    EXISTING_CONST,
+    INCOMING_CONST,
     MARCH,
     SEPT,
     COLUMNS
