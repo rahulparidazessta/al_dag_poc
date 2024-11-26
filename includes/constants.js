4e7158534de6741f595957c7827a1918ca2d74c1
rahulparidazessta <154420647+rahulparidazessta@users.noreply.github.com>
2024-11-26 11:35:07 +0530

    Update constants.js

--- includes/constants.js
+++ includes/constants.js
@@ -14,22 +14,6 @@ const SEPT = {
     FILE_FORMAT: "parquet"
 };
 
-const EXISTING_CONST = {
-  TABLE_NAME: dataform.projectConfig.vars.existing_name || "march", // Default: 'march'
-  DATASET_NAME: dataform.projectConfig.vars.existing_dataset_name || "airflow_demo", // Default: 'airflow_demo'
-  FOLDER_PATH: dataform.projectConfig.vars.existing_folder_path || "airflow_demo_zessta", // Default: 'airflow_demo_zessta'
-  FILE_NAME: dataform.projectConfig.vars.existing_file_name || "dataset1", // Default: 'dataset1'
-  FILE_FORMAT: dataform.projectConfig.vars.existing_file_format || "parquet" // Default: 'parquet'
-};
-
-const INCOMING_CONST = {
-  TABLE_NAME: dataform.projectConfig.vars.incoming_name || "march", // Default: 'march'
-  DATASET_NAME: dataform.projectConfig.vars.incoming_dataset_name || "airflow_demo", // Default: 'airflow_demo'
-  FOLDER_PATH: dataform.projectConfig.vars.incoming_folder_path || "airflow_demo_zessta", // Default: 'airflow_demo_zessta'
-  FILE_NAME: dataform.projectConfig.vars.incoming_file_name || "dataset2", // Default: 'dataset2'
-  FILE_FORMAT: dataform.projectConfig.vars.incoming_file_format || "csv" // Default: 'csv'
-};
-
 
 const COLUMNS = [
     "streetno",
@@ -46,8 +30,6 @@ const COLUMNS = [
 ];
 
 module.exports = {
-    EXISTING_CONST,
-    INCOMING_CONST,
     MARCH,
     SEPT,
     COLUMNS
